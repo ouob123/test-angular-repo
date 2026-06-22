dd033829c2dc19ee1c7dacf0162159349511b954
ouob123 <kyjin320@sookmyung.ac.kr>
2026-06-22 10:59:07 +0900

    version 4.0

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -40,7 +40,7 @@ export class CalculatorComponent implements OnInit {
       }
       case '-': {
 
-        this.result = firstInput + secondInput;
+        this.result = firstInput - secondInput;
         break;
 
       }
